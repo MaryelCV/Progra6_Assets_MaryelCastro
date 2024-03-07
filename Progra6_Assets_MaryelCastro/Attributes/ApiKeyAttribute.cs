e2bc8cbbc5e89a08bf286f2d183f2516f3cc23a9
MaryelCV <109971802+MaryelCV@users.noreply.github.com>
2024-03-07 14:21:05 -0600

    Semana 6

---
+++ Progra6_Assets_MaryelCastro/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+
+
+namespace Progra6_Assets_MaryelCastro.Attributes
+
+{
+
+    //con este atributo personalizado lo que se quiere es integrar una capa
+    //de seguridad por medio de una llave (clave:valor) 
+    //con esto se va a decorar ya sea todo un controller o solo 
+    //ciertos end points para que no se puedan usar a menos que se 
+    //integre la llave en el encabezado http que lo consume 
+
+
+
+    [AttributeUsage(validOn: AttributeTargets.All)]
+    public sealed class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        //indicamos cuál es la llave que se usará, esto estará en el 
+        //archivo appsettings.json 
+        private readonly string _apiKey = "P6ApiKey";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context,ActionExecutionDelegate next)
+        {
+
+            //en esta función validamos que el json que llega al WebAPI tenga los 
+            //datos del ApiKey válidos, caso contrario mostramos un error en el 
+            //response del endpoint
+            if (!context.HttpContext.Request.Headers.TryGetValue(_apiKey, out var ApiSalida))
+            {
+
+                //en el caso que no venga info de apikey en el header entonces....
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "The http request doesn't contain security information"
+                };
+                return;
+            }
+
+            //pero si viene la info de apikey se procede con....
+            //ahora que sabemos que la info de apikey viene, hay que validar que sea 
+            //la correcta. 
+
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+            var ApiKeyValue = appSettings.GetValue<string>(_apiKey);
+
+            //a este punto tenemos todo lo necesario para hacer la comparación de valores 
+
+            if (ApiKeyValue != null && !ApiKeyValue.Equals(ApiSalida))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Incorrect ApiKey Data.... "
+                };
+                return;
+            }
+
+            await next();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Progra6_Assets_MaryelCastro/Program.cs
+++ Progra6_Assets_MaryelCastro/Program.cs
@@ -1,23 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Progra6_Assets_MaryelCastro.Models;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        //Acá agregamos los valores de conexión a la base de da6tos para nuestra applicación 
+
+        //1. agregar la info en una una etiqueta en el archivo appsettins.json 
+
+        var CnnStrBuilder = new SqlConnectionStringBuilder(
+            builder.Configuration.GetConnectionString("CNNSTR")
+            );
+
+        //por seguridad es mejor agregar la info de la contraseña en este código o 
+        //podrímos tener un almacen externo. 
+        //Investigar qué son los User Secrets
+        CnnStrBuilder.Password = "progra6";
+
+        //ahora que hemos extraido la info de la cadena de conexión 
+        //creamos una variable tipo string que la almacene 
+        string CnnStr = CnnStrBuilder.ConnectionString;
+
+        //ahora conectamos el proyecto a la base de datos 
+        builder.Services.AddDbContext<Progra620241Context>(options => options.UseSqlServer(CnnStr));
+
+
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+        
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
